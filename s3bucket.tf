9efc881ca2a369657f1b45387ae7922335571528
sagar <saagarworkmail@gmail.com>
2025-07-03 11:53:08 +0530

    Update s3bucket.tf

--- s3bucket.tf
+++ s3bucket.tf
@@ -1,10 +1,10 @@
-# # S3 bucket created on AWS with the help of terraform using resource block
+# S3 bucket created on AWS with the help of terraform using resource block
 
-# resource "aws_s3_bucket" "mybucket"{
-#     bucket = "this-is-my-terraform-bucket"
+ resource "aws_s3_bucket" "mybucket"{
+     bucket = "this-is-my-terraform-bucket"
 
-#     tags = {
-#         name = "whatshouldicallit"
-#         environment = "dev"
-#     }
-# }
+     tags = {
+         name = "whatshouldicallit"
+         environment = "dev"
+}
+}
